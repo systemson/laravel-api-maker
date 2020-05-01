967d3611bd5a32be5982c633f2fe223908cab381
Deivi Peña <systemson@gmail.com>
2020-05-01 11:53:43 -0400

    Updates

--- src/ApiResourceTrait.php
+++ src/ApiResourceTrait.php
@@ -7,95 +7,119 @@ use Illuminate\Http\Request;
 
 trait ApiResourceTrait
 {
-	protected $perPage = 15;
+    protected $perPage = 20;
 
     protected function list(string $class, Request $request)
     {
-    	$listable = (new $class)->getListable();
+        $listable = (new $class)->getListable();
 
-    	$select = empty($listable) ? '*' : $listable;
+        $select = empty($listable) ? '*' : $listable;
 
-        $query = $class::select($select)
-        	->where($request->only($listable))
-        ;
+        // Se aplica el select.
+        $query = $class::select($select);
 
+        // se aplica el/los where
+        foreach ($listable as $column) {
+            if ($request->has($column)) {
+                $query->where($column, $request->get($column));
+            } elseif ($request->has($column . '_like')) {
+                $query->where($column, 'LIKE', '%' . $request->get($column . '_like') . '%');
+            } elseif ($request->has($column . '_not')) {
+                $query->where($column, '<>', $request->get($column . '_not'));
+            }
+        }
+
+        // Se aplica el orderBy
         if ($request->has('order_by') && ($order_by = $this->getOrderBy($request, $listable)) !== false && $this->validateOrderBy($order_by, $listable)) {
-        	$query->orderBy($order_by->column, $order_by->sort);
+            $query->orderBy($order_by->column, $order_by->sort);
+        }
+
+        // Se modifica la consulta antes de enviarla
+        if (method_exists($this, 'alterQuery')) {
+            $query = $this->alterQuery($query);
+        }
+
+        // Se aplica el per page
+        if ($request->get('per_page') == 0) {
+            $perPage = $query->count();
+        } else {
+            $perPage = $request->get('per_page') ?? $this->perPage;
         }
 
         $query_string = array_merge($listable, ['order_by', 'per_page']);
 
-        return $query->paginate(
-        	$request->get('per_page') ?? $this->perPage
-        )->appends($request->only($query_string));
+        return $query
+            ->paginate($perPage)
+            ->appends($request->only($query_string)
+        );
     }
 
     private function getOrderBy(Request $request)
     {
-    	$raw = $request->get('order_by');
+        $raw = $request->get('order_by');
 
-		$array = explode(':', $raw);
+        $array = explode(':', $raw);
 
-		if (isset($array[1]) && in_array(strtoupper($array[1]), ['ASC', 'DESC'])) {
-			$order = $array[1];
-		} else {
-			$order = 'ASC';
-		}
+        if (isset($array[1]) && in_array(strtoupper($array[1]), ['ASC', 'DESC'])) {
+            $order = $array[1];
+        } else {
+            $order = 'ASC';
+        }
 
-		return (object) [
-			'column' => $array[0],
-			'sort' => $order,
-		];
+        return (object) [
+            'column' => $array[0],
+            'sort' => $order,
+        ];
     }
 
     private function validateOrderBy($order_by, array $columns = [])
     {
-    	if (!empty($columns)) {
-    		return in_array($order_by->column, $columns);
-    	}
+        if (!empty($columns)) {
+            return in_array($order_by->column, $columns);
+        }
 
-    	false;
+        false;
     }
 
     protected function find(string $class, $id)
     {
-    	return $class::findOrFail($id);
+        return $class::findOrFail($id);
     }
 
     protected function new(string $class, Request $request)
     {
-    	$resource =  new $class;
+        $resource =  new $class;
 
-    	$input = $request->only($resource->getFillable());
+        $input = $request->only($resource->getFillable());
 
-    	$resource->fill($this->alterInput($input));
+        $resource->fill($this->alterInput($input));
 
-    	if ($resource->validate($request)) {
-    		$resource->save();
-    	} else {
-    		return abort(422);
-    	}
+        if ($resource->validate($request)) {
+            $resource->save();
+        } else {
+            return abort(422);
+        }
 
-    	return $resource;
+        return $resource;
     }
 
     protected function alterInput(array $input): array
     {
-    	return $input;
+        return $input;
     }
 
     protected function edit(Model $resource, Request $request)
     {
-    	$input = $request->only($resource->getFillable());
+        $input = $request->only($resource->getFillable());
 
-    	$resource->fill($this->alterInput($input));
+        $resource->fill($this->alterInput($input));
 
-    	if ($resource->validate($request)) {
-    		$resource->save();
-    	} else {
-    		return abort(422);
-    	}
+        if ($resource->validate($request)) {
+            $resource->save();
+        } else {
+            return abort(422);
+        }
 
-    	return $resource;
+        return $resource;
     }
 }
