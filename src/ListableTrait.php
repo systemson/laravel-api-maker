83425896de2101afe889fa0c7f2de43058307fe2
Deivi Peña <systemson@gmail.com>
2020-05-01 11:29:15 -0400

    Updates

---
+++ src/ListableTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Systemson\ApiMaker;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+trait ListableTrait
+{
+    /**
+     * Returns the listable attributes
+     *
+     * @return array
+     */
+    public function getListable(): array
+    {
+        return $this->listable;
+    }
+
+    /**
+     * Returns the selectable attributes
+     *
+     * @return array
+     */
+    public function getSelectable(): array
+    {
+        return $this->listable ?? '*';
+    }
+}
