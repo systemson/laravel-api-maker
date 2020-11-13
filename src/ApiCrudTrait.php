1c13f4924af98813fef96fa85ce5112627f264a2
Deivi Peña <32528630+systemson@users.noreply.github.com>
2020-11-13 15:40:12 -0400

    Update ApiCrudTrait.php

--- src/ApiCrudTrait.php
+++ src/ApiCrudTrait.php
@@ -2,59 +2,232 @@
 
 namespace Systemson\ApiMaker;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
-trait ApiCrudTrait
+trait ApiResourceTrait
 {
-    public function index(Request $request)
+    protected function list(string $class, Request $request)
     {
-        return response()->json(
-            $this->list(
-                $this->class,
-                $request
-            )
-        );
+        // New Model instance
+        $model = new $class;
+
+        // Get model's listable
+        $listable = $model->getListable();
+
+        // Get model's listable
+        $table = $model->getTable();
+
+        // Get columns to select
+        $query = $model->select($model->getSelectable());
+
+        // Filters - Set wheres
+        foreach ($listable as $key => $alias) {
+            list($name, $column) = $this->getNameAndColumn($key, $alias, $table);
+
+            if ($request->has($name)) {
+                if (is_array($value = $request->get($name)) || count($valueArray = explode(',', $value)) > 1) {
+                    $query->whereIn($column, $valueArray ?? $value);
+                    continue;
+                }
+
+                $query->where($column, $value);
+            } elseif ($request->has($name . '_not')) {
+                if (is_array($value = $request->get($name . '_not')) || count($valueArray = explode(',', $value)) > 1) {
+                    $query->whereNotIn($column, $valueArray ?? $value);
+                    continue;
+                }
+
+                $query->where($column, '<>', $value);
+            } elseif ($request->has($name . '_like')) {
+                $query->whereRaw("UPPER({$column}) LIKE UPPER('%" . $request->get($name . '_like') . "%')");
+            } elseif ($request->has($name . '_between')) {
+                $value = $request->get($name . '_between');
+
+                $valueArray = !is_array($value) ? explode(',', $value) : $value;
+
+                $query->whereBetween($column, $valueArray);
+            }
+        }
+
+        // Filters by relations
+        if ($request->has('has')) {
+            if ($has = json_decode($request->get('has'), true)) {
+                foreach ($has as $name => $values) {
+                    list($relation, $column) = explode('.', $name);
+                        $query->whereHas($relation, function ($query) use ($column, $values) {
+                            $query->whereIn("{$query->getModel()->getTable()}.{$column}", $values);
+                        });
+                }
+            }
+        }
+
+        // Set order by
+        if ($request->has('order_by')) {
+            $orderByArray = explode(',', $request->get('order_by'));
+
+            foreach ($orderByArray as $orderByRaw) {
+                if (
+                   ($orderBy = $this->getOrderBy($orderByRaw, $listable)) !== false
+                    &&
+                    $this->validateOrderBy($orderBy, $model, $listable)
+                ) {
+                    $query = $this->applyOrderBy($query, $orderBy);
+                }
+            }
+        }
+
+        // Load relations
+        $query->with($this->getEagerLoadedRelations($request, $class));
+
+        // Alter the query before getting the items
+        if (method_exists($this, 'alterQuery')) {
+            $query = $this->alterQuery($query);
+        }
+
+        $perPage = $request->get('per_page') ?? $this->perPage ?? 20;
+
+        // Set per_page
+        if ($perPage == 0) {
+            $perPage = $query->count();
+        }
+
+        $query_string = array_merge($listable, ['order_by', 'per_page', 'with']);
+
+        $paginated = $query
+            ->paginate($perPage)
+            ->appends($request->only($query_string))
+        ;
+
+        $items = collect($paginated->items())
+            ->each(function ($resource) use ($request) {
+                $resource->append($this->getAppendableAttributes($request));
+            })
+        ;
+
+        return $paginated;
     }
 
-    public function store(Request $request)
+    private function getAppendableAttributes(Request $request)
     {
-        $resource = $this->new(
-            $this->class,
-            $request
-        );
-
-        return response($resource, 201);
+        if (!$request->has('append')) {
+            return [];
+        }
+        return explode(',', $request->get('append'));
     }
 
-    public function show($id)
+    private function getNameAndColumn($key, $alias, $table)
     {
-        return response()->json(
-            $this->find(
-                $this->class,
-                $id
-            )
-        );
+        $column = !is_numeric($key) ? $key : $alias;
+
+        if ($alias == $column) {
+            return [
+                // Name
+                $alias,
+                // Column
+                "{$table}.{$column}",
+            ];
+        }
+
+        return [
+            $alias,
+            $column,
+        ];
     }
 
-    public function update($id, Request $request)
+    private function getOrderBy($orderBy)
     {
-        $resource = $this->edit(
-            $this->class::findOrFail($id),
-            $request
-        );
+        $array = explode(':', $orderBy);
 
-        return response($resource);
+        return (object) [
+            'column' => $array[0],
+            'order' => $array[1] ?? 'ASC',
+        ];
     }
 
-    public function destroy($id)
+    private function validateOrderBy($order_by, $model, array $columns = [])
     {
-        $resource = $this->class::findOrFail($id);
+        if (empty($columns) || !in_array(strtoupper($order_by->order), ['ASC', 'DESC'])) {
+            return false;
+        }
 
-        $resource->delete();
+        if (in_array($order_by->column, $columns)) {
+            return true;
+        } elseif (count($name_array = explode('.', $order_by->column)) == 2) {
+            return method_exists($model, $name_array[0]);
+        }
 
-        return response([
-            'status' => 'deleted',
-            'message' => 'The resource was successfully deleted',
-        ]);
+        return false;
+    }
+
+    private function applyOrderBy($query, $order_by)
+    {
+        if (count($columnaArray = explode('.', $order_by->column)) == 2) {
+            $model = $query->getModel();
+            $relation = $model->{$columnaArray[0]}();
+
+            return $query->join(
+                $table = $relation->getRelated()->getTable(),
+                $relation->getQualifiedForeignKeyName(),
+                $relation->getQualifiedOwnerKeyName()
+            )->orderBy("{$table}.{$columnaArray[1]}", $order_by->order);
+        }
+
+        return $query->orderBy($order_by->column, $order_by->order);
+    }
+
+    private function getEagerLoadedRelations(Request $request, $model)
+    {
+        return collect(explode(',', $request->get('with')))->filter(
+            function ($relation) use ($model) {
+                $relationArray = explode('.', $relation);
+                return method_exists($model, current($relationArray));
+            }
+        )->toArray();
+    }
+
+    protected function find(string $class, $id, $pk = null)
+    {
+        $model = (new $class);
+
+        if (!is_null($pk)) {
+            $model->setKeyName($pk);
+        }
+
+        return $model->select($model->getSelectable())
+            ->with($this->getEagerLoadedRelations(request(), $class))
+            ->findOrFail($id)
+            ->append($this->getAppendableAttributes(request()))
+        ;
+    }
+
+    protected function new(string $class, Request $request)
+    {
+        $resource = new $class;
+
+        $input = $request->only($resource->getFillable());
+
+        $resource->fill($this->alterInput($input));
+
+        $resource->save();
+
+        return $resource->refresh();
+    }
+
+    protected function alterInput(array $input): array
+    {
+        return $input;
+    }
+
+    protected function edit(Model $resource, Request $request)
+    {
+        $input = $request->only($resource->getFillable());
+
+        $resource->fill($this->alterInput($input));
+
+        $resource->save();
+
+        return $resource->fresh();
     }
 }
